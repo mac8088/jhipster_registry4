98980bf8fcf5ea73d573bb03c5e455c0b86bd6a3
Ma Chun <chun.ma@qq.com>
2019-11-29 21:25:23 +0800

    changed

--- run-jhipster_registry.bat
+++ run-jhipster_registry.bat
@@ -2,4 +2,4 @@
 @echo	===================== JHipster Registry===================
 @echo	==========================================================
 title JHipster Registry
-java -jar jhipster-registry-4.0.6.war --spring.security.user.password=admin --jhipster.security.authentication.jwt.secret=my-secret-key-which-should-be-changed-in-production-and-be-base64-encoded --spring.cloud.config.server.composite.0.type=native --spring.cloud.config.server.composite.0.search-locations=file:./central-config
+java -jar jhipster-registry.war --spring.security.user.password=admin --jhipster.security.authentication.jwt.secret=my-secret-key-which-should-be-changed-in-production-and-be-base64-encoded --spring.cloud.config.server.composite.0.type=native --spring.cloud.config.server.composite.0.search-locations=file:./central-config
